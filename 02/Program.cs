02a1392155cec1fe309be651d35f7d9a70e9ac25
AnkaBigudi <ankabigudi@ya.ru>
2023-02-25 22:37:51 +0300

    Цикл в цикле, цикл for

---
+++ 02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Цикл for - цикл со счетчиком, когда знаем кол-во итераций
+string Method4(int count, string text){
+  string result = string.Empty;
+  for(int i = 0; i < count; i++){ //инициальзация, проверка условия, счетчик
+    result = result + text;
+  }
+return result;
+}
+string res = Method4(10, "Текст");
+Console.WriteLine(res);
+
+//Цикл в цикле на примере таблицы умножения:
+for(int i = 2; i <= 10; i++){
+  for(int j = 2; j <= 10; j++){ //тело внешнего цикла
+    Console.WriteLine($"{i} x {j} = {i * j}"); //тело внешнего цикла
+  }
+  Console.WriteLine(); // переход на новую строку после одного цикла, чтоб красиво было
+}
